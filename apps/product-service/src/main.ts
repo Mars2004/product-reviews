92e7d0ac56ade0b2e517c5f577f64d4b3d5f0634
Martin Svoboda <martin.svoboda@marstech.cz>
2024-05-06 11:23:49 +0200

    msv-1: add swagger support

--- apps/product-service/src/main.ts
+++ apps/product-service/src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const appVersion = require('../../../package.json').version;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // get the ConfigService instance
+  const configService = app.get<ConfigService>(ConfigService);
+
+  // check if application is in development or stage mode
+  const nodeEnv = configService.get<string>('NODE_ENV');
+  if (nodeEnv === 'development') {
+    // enable swagger
+    const config = new DocumentBuilder()
+      .setTitle('Product Reviews API')
+      .setDescription('Test product reviews project REST API description')
+      .setVersion(appVersion)
+      .build();
+
+    // create Swagger document
+    const document = SwaggerModule.createDocument(app, config);
+    document.security = [{ api_key: [] }];
+    SwaggerModule.setup('swagger', app, document);
+  }
+
+  // start application on given port
+  await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
